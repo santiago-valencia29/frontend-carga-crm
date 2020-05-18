c0d7563b114268759f20c63b634a14a7b92136e8
edsvalen <edsvalen@bancolombia.com.co>
2020-05-18 10:19:47 -0500

    fix service delete import

--- src/services/campana.service.ts
+++ src/services/campana.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { Ferreteria } from '../../models/dyrcocinas/ferreteria.model';
 import { ClienteCrm } from '../models/cliente-crm.model';
 
 @Injectable({
